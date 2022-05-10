22a5bb3a6d160baa040f532c91aaf32eae52969f
bhaushedbale <44108885+bhaushedbale@users.noreply.github.com>
2022-05-10 16:49:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlite.text(fruityvice_response)
+streamlit.text(fruityvice_response)
 
 
